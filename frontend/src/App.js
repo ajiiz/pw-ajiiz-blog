8bb9f924e5a9498ffc0d120f39b9bdfe773f4082
ajiiz <opticpiter@gmail.com>
2020-12-15 10:43:30 +0100

    Added default Routing for components in App Component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,21 @@
 import React from 'react'
+import { Route, BrowserRouter as Router, Switch, Redirect } from 'react-router-dom'
+import Home from './components/HomePage/Home'
+import Login from './components/LoginPage/Login'
+import Blog from './components/BlogPage/Blog'
 
 const App = () => {
     return (
-        <div>
-            <h1>Init</h1>
-        </div>
+        <Router>
+            <Switch>
+                <Route exact path="/">
+                    <Redirect to="/home"></Redirect>
+                </Route>
+                <Route exact path="/home" component={Home} />
+                <Route exact path="/login" component={Login} />
+                <Route exact path="/blog" component={Blog} />
+            </Switch>
+        </Router>
     )
 }
 
