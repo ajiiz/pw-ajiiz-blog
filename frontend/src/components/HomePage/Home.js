cfe51f914cb1dbeec9ae2b264f9fe3a23eb727f2
ajiiz <opticpiter@gmail.com>
2020-12-15 20:17:40 +0100

    Added custom cursor to Home page

--- frontend/src/components/HomePage/Home.js
+++ frontend/src/components/HomePage/Home.js
@@ -1,10 +1,14 @@
 import React from 'react'
+import CustomCursor from '../CustomCursor/CustomCursor'
 
 const Home = () => {
     return (
+        <>
+        <CustomCursor />
         <div>
             Home
         </div>
+        </>
     )
 }
 
