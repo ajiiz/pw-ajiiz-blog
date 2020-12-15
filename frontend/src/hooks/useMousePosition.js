0b826f4a387e5b76f58d9fd339826371ed56b922
ajiiz <opticpiter@gmail.com>
2020-12-15 19:59:03 +0100

    Fixed spacing, removed unused data and modified initial x and y in MousePosition hook

--- frontend/src/hooks/useMousePosition.js
+++ frontend/src/hooks/useMousePosition.js
@@ -1,31 +1,31 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect } from "react"
 
 const useMousePosition = () => {
 
-  const [position, setPosition] = useState({
-    clientX: 0,
-    clientY: 0,
-  });
-
-  const updatePosition = event => {
-    const { pageX, pageY, clientX, clientY } = event
-    setPosition({
-      clientX,
-      clientY,
+    const [position, setPosition] = useState({
+        clientX: -40,
+        clientY: -40,
     })
-  }
 
-  useEffect(() => {
-    document.addEventListener("mousemove", updatePosition, false)
-    document.addEventListener("mouseenter", updatePosition, false)
+    const updatePosition = event => {
+        const { clientX, clientY } = event
+        setPosition({
+            clientX,
+            clientY,
+        })
+    }
+
+    useEffect(() => {
+        document.addEventListener("mousemove", updatePosition, false)
+        document.addEventListener("mouseenter", updatePosition, false)
 
     return () => {
-      document.removeEventListener("mousemove", updatePosition)
-      document.removeEventListener("mouseenter", updatePosition)
+        document.removeEventListener("mousemove", updatePosition)
+        document.removeEventListener("mouseenter", updatePosition)
     }
-  }, [])
+    }, [])
 
-  return position
+    return position
 }
 
 export default useMousePosition
