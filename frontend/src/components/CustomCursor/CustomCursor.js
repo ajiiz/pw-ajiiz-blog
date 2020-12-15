b27aef76b2b1a329599329df02056350a87cc5b3
ajiiz <opticpiter@gmail.com>
2020-12-15 20:16:25 +0100

    Added custom cusror with styling and used previously created useMousePosition hook

--- frontend/src/components/CustomCursor/CustomCursor.js
+++ frontend/src/components/CustomCursor/CustomCursor.js
@@ -1,7 +1,27 @@
-import React from 'react'
+import React from "react"
+import useMousePosition from "../../hooks/useMousePosition"
 
 const CustomCursor = () => {
-    return <div className="custom-cursor"></div>
+
+    const { clientX, clientY } = useMousePosition()
+
+    return (
+        <div style={{
+            position: "fixed",
+            top: clientY - 20,
+            left: clientX - 20,
+            zIndex: 9999,
+            pointerEvents: "none",
+            borderRadius: "50%",
+            border: "1px solid #E2E2E2",
+            width: "35px",
+            height: "35px"
+        }}>
+            <svg style={{ position: "absolute", top: -8, left: -8 }}>
+                <circle cx="25" cy="25" r="5" fill="#151515"></circle>
+            </svg>
+        </div>
+    )
 }
 
 export default CustomCursor
