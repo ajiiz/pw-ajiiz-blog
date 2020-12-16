c35b725b554ade8f8080c8ae72d8436c91cada7f
ajiiz <opticpiter@gmail.com>
2020-12-16 21:54:32 +0100

    Added loading counter that is increasing until 100 using useEffect and setInterval in Loading Component

--- frontend/src/components/LoadingPage/Loading.js
+++ frontend/src/components/LoadingPage/Loading.js
@@ -1,16 +1,32 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import BannerLight from '../../assets/LoadingTextHeader-light.svg'
 import BannerDark from '../../assets/LoadingTextHeader-dark.svg'
 import '../../styles/loading.scss'
 
 const Loading = () => {
+
+    const [count, setCount] = useState(1)
+
+    useEffect(() => {
+        let i = 1
+        const interval = setInterval(() => {
+            i += (Math.floor(Math.random() * 2) + 1)
+            if (i <= 100) {
+                setCount(i)
+            } else {
+                setCount(100)
+                clearInterval(interval)
+            }
+        }, 60)
+    }, [])
+
     return (
         <div className="loading">
             <div className="loading__text">
                 <h1 className="loading__text__header">
                 HOLD ON, YOUR WEB EXPERIENCE IS
                 <span className="loading__text__header-reversed"> LOADING</span>
-                <span className="loading__text__header__numbers">1-100</span>
+                <span className="loading__text__header__numbers">{ count}-100</span>
                 </h1>
                 <p className="loading__text__info">
                     please wait a few seconds.
