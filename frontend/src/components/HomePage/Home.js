dd0338885811b2aed4fcecfbe2d33d1da3569204
ajiiz <opticpiter@gmail.com>
2020-12-16 17:22:14 +0100

    Imported Loading Component to Home Component

--- frontend/src/components/HomePage/Home.js
+++ frontend/src/components/HomePage/Home.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import CustomCursor from '../CustomCursor/CustomCursor'
+import Loading from '../LoadingPage/Loading'
 
 const Home = () => {
     return (
         <>
         <CustomCursor />
+        <Loading />
         <div>
             Home
         </div>
