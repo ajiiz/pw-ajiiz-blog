93c94c9341f00d8c35d4c80e8998241e609b5f81
ajiiz <opticpiter@gmail.com>
2020-12-16 16:07:57 +0100

    Added basic elements in Loading Component

--- frontend/src/components/LoadingPage/Loading.js
+++ frontend/src/components/LoadingPage/Loading.js
@@ -1,9 +1,30 @@
 import React from 'react'
+import BannerLight from '../../assets/LoadingTextHeader-light.svg'
+import BannerDark from '../../assets/LoadingTextHeader-dark.svg'
+import '../../styles/loading.scss'
 
 const Loading = () => {
     return (
-        <div>
-            Loading
+        <div className="loading">
+            <div className="loading__text">
+                <h1 className="loading__text__header">
+                    <span className="loading__text__header__numbers">
+                        1-100
+                    </span>
+                HOLD ON, YOUR WEB EXPERIENCE IS
+                    <span className="loading__text__header loading__text__header-reversed">
+                        LOADING
+                    </span>
+                </h1>
+                <p className="loading__text__info">
+                    please wait a few seconds.
+                </p>
+            </div>
+            <div className="loading__banners">
+                <div className="loading__banners__banner loading__banners__banner-dark"><img src={ BannerDark } alt="banner"></img></div>
+                <div className="loading__banners__banner loading__banners__banner-light"><img src={ BannerLight } alt="banner"></img></div>
+                <div className="loading__banners__banner loading__banners__banner-dark"><img src={ BannerDark } alt="banner"></img></div>
+            </div>
         </div>
     )
 }
