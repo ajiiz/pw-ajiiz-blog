5cdf4d0819b57ec2479fc6eb2437696768d29059
ajiiz <opticpiter@gmail.com>
2020-12-17 20:13:02 +0100

    Imported gsap, useref and added props in loading

--- frontend/src/components/LoadingPage/Loading.js
+++ frontend/src/components/LoadingPage/Loading.js
@@ -1,11 +1,13 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect, useRef } from 'react'
 import BannerLight from '../../assets/LoadingTextHeader-light.svg'
 import BannerDark from '../../assets/LoadingTextHeader-dark.svg'
+import { Power1, gsap } from 'gsap'
 import '../../styles/loading.scss'
 
-const Loading = () => {
+const Loading = ({ handleAnimationStart }) => {
 
     const [count, setCount] = useState(1)
+    let container = useRef(null)
 
     useEffect(() => {
         let i = 1
@@ -15,13 +17,14 @@ const Loading = () => {
                 setCount(i)
             } else {
                 setCount(100)
+                handleAnimationStart()
                 clearInterval(interval)
             }
         }, 60)
     }, [])
 
     return (
-        <div className="loading">
+        <div className="loading" ref= {el => container = el}>
             <div className="loading__text">
                 <div className="loading__text__header">
                     <span className="loading__text__header__main">HOLD ON, YOUR WEB EXPERIENCE IS </span>
