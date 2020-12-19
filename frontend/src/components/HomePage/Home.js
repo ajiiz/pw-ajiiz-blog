be4b8be59ea92a98988858703f0833dbfd85f66e
ajiiz <opticpiter@gmail.com>
2020-12-19 18:36:44 +0100

    Changed state method name

--- frontend/src/components/HomePage/Home.js
+++ frontend/src/components/HomePage/Home.js
@@ -1,14 +1,13 @@
-import React, { useEffect, useState } from 'react'
+import React, { useState } from 'react'
 import CustomCursor from '../CustomCursor/CustomCursor'
 import Loading from '../LoadingPage/Loading'
 
 const Home = () => {
 
-    const [shouldLoad, isShouldLoad] = useState(false)
+    const [shouldLoad, setShouldLoad] = useState(false)
 
     const handleAnimationStart = () => {
-        isShouldLoad(!shouldLoad)
-        console.log("changed")
+        setShouldLoad(!shouldLoad)
     }
 
     return (
