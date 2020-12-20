00568ce5da39ac87c2514de821ca3a3adc9a28d1
ajiiz <opticpiter@gmail.com>
2020-12-20 22:08:34 +0100

    Added elements including NavLink to change paths in Navbar

--- frontend/src/components/Navbar/Navbar.js
+++ frontend/src/components/Navbar/Navbar.js
@@ -1,10 +1,28 @@
 import React from 'react'
 import "../../styles/navbar.scss"
+import Logo from "../../assets/Logo.png"
+import { NavLink } from "react-router-dom"
+import styled from 'styled-components'
+
+const StyledLink = styled(NavLink)`
+color: inherit;
+&:focus, &:hover, &:visited, &:link, &:active {
+    text-decoration: none;
+}
+`
 
 const Navbar = () => {
     return (
-        <div>
-
+        <div className="navbar">
+            <img className="navbar__logo" src={Logo}></img>
+            <ul className="navbar__list">
+                <StyledLink to="/home">
+                    <li className="navbar__list__item">Home</li>
+                </StyledLink>
+                <StyledLink to="/blog">
+                    <li classList="navbar__list__item">Blog</li>
+                </StyledLink>
+            </ul>
         </div>
     )
 }
