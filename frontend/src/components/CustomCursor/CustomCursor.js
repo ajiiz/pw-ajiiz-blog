e05112394fb0507ba572652279798aed38839293
ajiiz <opticpiter@gmail.com>
2020-12-23 17:12:08 +0100

    Added classname

--- frontend/src/components/CustomCursor/CustomCursor.js
+++ frontend/src/components/CustomCursor/CustomCursor.js
@@ -16,7 +16,7 @@ const CustomCursor = () => {
             border: "1px solid #E2E2E2",
             width: "35px",
             height: "35px"
-        }}>
+        }} className="cursor">
             <svg style={{ position: "absolute", top: -8, left: -8 }}>
                 <circle cx="25" cy="25" r="5" fill="#151515"></circle>
             </svg>
