66b59f611fd1feac3854f29dc07c35c39a299396
ajiiz <opticpiter@gmail.com>
2021-02-04 00:37:29 +0100

    Styled NavLink using styled components in CustomLink

--- frontend/src/components/Navbar/CustomLink.js
+++ frontend/src/components/Navbar/CustomLink.js
@@ -2,6 +2,14 @@ import React from 'react'
 import { NavLink, useHistory } from "react-router-dom"
 import styled from "styled-components"
 
+const StyledLink = styled(NavLink)`
+color: inherit;
+padding: 0 15px 0 15px;
+&:focus, &:hover, &:visited, &:link, &:active {
+    text-decoration: none;
+}
+`
+
 const CustomLink = ({ to, name }) => {
     return (
         <StyledLink to={to} onClick={delayedLinkPush}>
