9db4494c58a3935ba6c2443f14066e331f424878
ajiiz <opticpiter@gmail.com>
2021-02-04 00:33:23 +0100

    Removed unnecessary imports and imported CustomLink

--- frontend/src/components/Navbar/Navbar.js
+++ frontend/src/components/Navbar/Navbar.js
@@ -1,28 +1,14 @@
 import React from "react"
 import Logo from "../../assets/logo.png"
-import { NavLink } from "react-router-dom"
-import styled from "styled-components"
 import "../../styles/navbar.scss"
-
-const StyledLink = styled(NavLink)`
-color: inherit;
-padding: 0 15px 0 15px;
-&:focus, &:hover, &:visited, &:link, &:active {
-    text-decoration: none;
-}
-`
+import CustomLink from "./CustomLink"
 
 const Navbar = () => {
     return (
         <div className="navbar">
             <img className="navbar__logo" src={Logo} alt="logo"></img>
             <ul className="navbar__list">
-                <StyledLink to="/home">
-                    <li className="navbar__list__item">home</li>
-                </StyledLink>
-                <StyledLink to="/blog">
-                    <li className="navbar__list__item">blog</li>
-                </StyledLink>
+
             </ul>
         </div>
     )
