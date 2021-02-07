855a3d6f41d7e1719fa45debd41a37cd1d2c98b4
ajiiz <opticpiter@gmail.com>
2021-02-07 23:09:04 +0100

    Imported gsap and hooks

--- frontend/src/components/BlogPage/Blog.js
+++ frontend/src/components/BlogPage/Blog.js
@@ -1,8 +1,10 @@
-import React from "react"
+import React, { useEffect, useRef }  from "react"
 import Navbar from "../Navbar/Navbar"
 import "../../styles/blog.scss"
+import { Power1, gsap } from "gsap"
 
 const Blog = () => {
+
     return (
         <>
             <Navbar />
