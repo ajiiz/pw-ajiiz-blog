7b1100485e29330847e380d54742b8a2c3bb5272
ajiiz <opticpiter@gmail.com>
2021-02-07 23:18:04 +0100

    Added props method for animation and sent it to CustomLink

--- frontend/src/components/Navbar/Navbar.js
+++ frontend/src/components/Navbar/Navbar.js
@@ -3,13 +3,13 @@ import Logo from "../../assets/logo.png"
 import "../../styles/navbar.scss"
 import CustomLink from "./CustomLink"
 
-const Navbar = () => {
+const Navbar = ({ handleAnimationClose }) => {
     return (
         <div className="navbar">
             <img className="navbar__logo" src={Logo} alt="logo"></img>
             <ul className="navbar__list">
-                <CustomLink to="/home" name="home" />
-                <CustomLink to="/blog" name="blog" />
+                <CustomLink to="/home" name="home" handleAnimationClose={handleAnimationClose} />
+                <CustomLink to="/blog" name="blog" handleAnimationClose={handleAnimationClose} />
             </ul>
         </div>
     )
