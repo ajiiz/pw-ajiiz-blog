aab9a09ad3caac6012086aefe6ad2cfa094b7eab
ajiiz <opticpiter@gmail.com>
2021-02-14 00:42:40 +0100

    Added scroll-up element

--- frontend/src/components/BlogPage/Blog.js
+++ frontend/src/components/BlogPage/Blog.js
@@ -24,18 +24,19 @@ const Blog = () => {
             <Navbar handleAnimationClose={handleAnimationClose} />
             <div className="blog" ref= {el => container = el}>
             <div className="banner"><img src={BannerDark} alt="banner"></img></div>
-                <div className="blog__header">
-                    <h1 className="blog__header__text">Welcome to my mandness</h1>
+            <div className="blog__header">
+                <h1 className="blog__header__text">Welcome to my mandness</h1>
+            </div>
+            <div className="blog__posts">
+            {/* There will be mapping through posts */}
+                <div className="blog__posts__post">
+                    <div className="blog__posts__post__date">date</div>
+                    <h2 className="blog__posts__post__title">title</h2>
+                    <p className="blog__posts__post__content">content</p>
                 </div>
-                <div className="blog__posts">
-                {/* There will be mapping through posts */}
-                    <div className="blog__posts__post">
-                        <div className="blog__posts__post__date">date</div>
-                        <h2 className="blog__posts__post__title">title</h2>
-                        <p className="blog__posts__post__content">content</p>
-                    </div>
-                {/* ------------ */}
-                </div>
+            {/* ------------ */}
+            </div>
+            <div className="blog__scroll-up"></div>
             <div className="banner"><img src={BannerDark} alt="banner"></img></div>
             </div>
         </>
