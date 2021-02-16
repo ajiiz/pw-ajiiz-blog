22882976754a76d623637069e6d7f94162106c88
ajiiz <opticpiter@gmail.com>
2021-02-16 14:57:52 +0100

    Added wrapper for blog content

--- frontend/src/components/BlogPage/Blog.js
+++ frontend/src/components/BlogPage/Blog.js
@@ -24,24 +24,26 @@ const Blog = () => {
             <Navbar handleAnimationClose={handleAnimationClose} />
             <div className="blog" ref= {el => container = el}>
                 <div className="banner"><img src={BannerDark} alt="banner"></img></div>
-                <div className="blog__header">
-                    <h1 className="blog__header__text">Welcome to my mandness</h1>
-                </div>
-                <div className="blog__posts">
-                {/* There will be mapping through posts */}
-                    <div className="blog__posts__post">
-                        <div className="blog__posts__post__image">
-                            <image></image>
-                        </div>
-                        <div className="blog__posts__post__content">
-                            <div className="blog__posts__post__content__date">date</div>
-                            <h2 className="blog__posts__post__content__title">title</h2>
-                            <p className="blog__posts__post__content__details">content</p>
-                        </div>
+                <div className="blog__wrapper">
+                    <div className="blog__wrapper__header">
+                        <h1 className="blog__wrapper__header__text">Welcome to my mandness</h1>
                     </div>
-                {/* ------------ */}
+                    <div className="blog__wrapper__posts">
+                    {/* There will be mapping through posts */}
+                        <div className="blog__wrapper__posts__post">
+                            <div className="blog__wrapper__posts__post__image">
+                                <image></image>
+                            </div>
+                            <div className="blog__wrapper__posts__post__content">
+                                <div className="blog__wrapper__posts__post__content__date">date</div>
+                                <h2 className="blog__wrapper__posts__post__content__title">title</h2>
+                                <p className="blog__wrapper__posts__post__content__details">content</p>
+                            </div>
+                        </div>
+                    {/* ------------ */}
+                    </div>
+                    <div className="blog__wrapper__scroll-up"></div>
                 </div>
-                <div className="blog__scroll-up"></div>
                 <div className="banner"><img src={BannerDark} alt="banner"></img></div>
             </div>
         </>
