1a3142b119fa96374d0e4de89862fd3fef1d2ff1
ajiiz <opticpiter@gmail.com>
2021-02-20 13:07:51 +0100

    Imported styles and navbar

--- frontend/src/components/LoginPage/Login.js
+++ frontend/src/components/LoginPage/Login.js
@@ -1,32 +1,37 @@
 import React from "react"
+import Navbar from "../Navbar/Navbar"
+import "../../styles/login.scss"
 
 const Login = () => {
     return (
-        <div className="login">
-            <p className="login__header">Thats a secrect area</p>
-            <form className="login__form">
-                <label>
-                    Username
+        <>
+            <Navbar />
+            <div className="login">
+                <p className="login__header">Thats a secrect area</p>
+                <form className="login__form">
+                    <label>
+                        Username
+                        <input
+                            type="text"
+                            name="username"
+                            value="{username}"
+                        />
+                    </label>
+                    <label>
+                        Password
+                        <input
+                            type="password"
+                            name="password"
+                            value="{password}"
+                        />
+                    </label>
                     <input
-                        type="text"
-                        name="username"
-                        value="{username}"
+                        type="submit"
+                        value="sign in"
                     />
-                </label>
-                <label>
-                    Password
-                    <input
-                        type="password"
-                        name="password"
-                        value="{password}"
-                    />
-                </label>
-                <input
-                    type="submit"
-                    value="sign in"
-                />
-            </form>
-        </div>
+                </form>
+            </div>
+        </>
     )
 }
 
