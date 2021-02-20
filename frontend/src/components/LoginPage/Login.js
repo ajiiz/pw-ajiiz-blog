b80b6eb1e1c606966060ee676e9d09e204c25ccc
ajiiz <opticpiter@gmail.com>
2021-02-20 13:04:01 +0100

    Added elements

--- frontend/src/components/LoginPage/Login.js
+++ frontend/src/components/LoginPage/Login.js
@@ -2,8 +2,30 @@ import React from "react"
 
 const Login = () => {
     return (
-        <div>
-            Login
+        <div className="login">
+            <p className="login__header">Thats a secrect area</p>
+            <form className="login__form">
+                <label>
+                    Username
+                    <input
+                        type="text"
+                        name="username"
+                        value="{username}"
+                    />
+                </label>
+                <label>
+                    Password
+                    <input
+                        type="password"
+                        name="password"
+                        value="{password}"
+                    />
+                </label>
+                <input
+                    type="submit"
+                    value="sign in"
+                />
+            </form>
         </div>
     )
 }
