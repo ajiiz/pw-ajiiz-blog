98de5442bea3df5853248a059f6b2da6ae32c9cb
ajiiz <opticpiter@gmail.com>
2021-02-21 15:44:38 +0100

    Improted gsap and useEffect

--- frontend/src/components/LoginPage/Login.js
+++ frontend/src/components/LoginPage/Login.js
@@ -1,6 +1,7 @@
-import React, { useState } from "react"
+import React, { useState, useEffect } from "react"
 import Navbar from "../Navbar/Navbar"
 import BannerDark from "../../assets/banner-dark.svg"
+import { Power1, gsap } from "gsap"
 import "../../styles/login.scss"
 
 const Login = () => {
