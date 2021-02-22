a24b3cf205bd0c121e1669fd55d61de7518439f5
ajiiz <opticpiter@gmail.com>
2021-02-22 23:53:23 +0100

    Added banner, navbar and initialized close animation method

--- frontend/src/components/PostPage/Post.js
+++ frontend/src/components/PostPage/Post.js
@@ -5,10 +5,20 @@ import { Power1, gsap } from "gsap"
 import "../../styles/post.scss"
 
 const Post = () => {
+
+    const handleAnimationClose = () => {
+        //There goes animation when changing the page
+    }
+
     return (
-        <div>
+        <>
+        <Navbar handleAnimationClose={handleAnimationClose} />
+        <div className="banner"><img src={BannerDark} alt="banner"></img></div>
+        <div className="post">
             Post
         </div>
+        <div className="banner"><img src={BannerDark} alt="banner"></img></div>
+        </>
     )
 }
 
