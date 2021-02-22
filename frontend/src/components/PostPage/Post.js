c39fd30eac68482e2f4eff2243e9c580ba238a73
ajiiz <opticpiter@gmail.com>
2021-02-22 23:50:37 +0100

    Added imports

--- frontend/src/components/PostPage/Post.js
+++ frontend/src/components/PostPage/Post.js
@@ -1,4 +1,8 @@
 import React from "react"
+import Navbar from "../Navbar/Navbar"
+import BannerDark from "../../assets/banner-dark.svg"
+import { Power1, gsap } from "gsap"
+import "../../styles/post.scss"
 
 const Post = () => {
     return (
