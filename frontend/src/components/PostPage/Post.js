9f269a591aee803c96f759ffc2fd1527e86d88d7
ajiiz <opticpiter@gmail.com>
2021-02-24 17:04:33 +0100

    Added elements with classNames

--- frontend/src/components/PostPage/Post.js
+++ frontend/src/components/PostPage/Post.js
@@ -13,11 +13,48 @@ const Post = () => {
     return (
         <>
         <Navbar handleAnimationClose={handleAnimationClose} />
-        <div className="banner"><img src={BannerDark} alt="banner"></img></div>
         <div className="post">
-            Post
-        </div>
-        <div className="banner"><img src={BannerDark} alt="banner"></img></div>
+                <div className="banner"><img src={BannerDark} alt="banner"></img></div>
+                <div className="post__wrapper">
+                    <p className="post__wrapper__header">Thats a secrect area</p>
+                    <form className="post__wrapper__form">
+                        <p className="post__wrapper__form__name">
+                            Title
+                        </p>
+                        <input
+                            className="input-text post__wrapper__form__input"
+                            type="text"
+                            placeholder="username"
+                            name="title"
+                            value=""
+                        />
+                        <p className="post__wrapper__form__name">
+                            Content
+                        </p>
+                        <input
+                            className="input-text post__wrapper__form__input"
+                            type="text"
+                            placeholder=""
+                            name="content"
+                            value=""
+                        />
+                        <p className="post__wrapper__form__name">
+                            Image
+                        </p>
+                        <input
+                            className="input-submit post__wrapper__form__image"
+                            type="file"
+                            name="image"
+                        />
+                        <input
+                            className="input-submit post__wrapper__form__submit"
+                            type="submit"
+                            value="post"
+                        />
+                    </form>
+                </div>
+                <div className="banner"><img src={BannerDark} alt="banner"></img></div>
+            </div>
         </>
     )
 }
