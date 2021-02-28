2106b9ca277a5f4c587d69f0c5f44bb52ae6e6b0
ajiiz <opticpiter@gmail.com>
2021-02-28 19:45:09 +0100

    Modified styles for custom cursor - added styled-components cursor with props (X and Y of the cursor)

--- frontend/src/components/CustomCursor/CustomCursor.js
+++ frontend/src/components/CustomCursor/CustomCursor.js
@@ -1,24 +1,30 @@
 import React from "react"
 import useMousePosition from "../../hooks/useMousePosition"
+import styled from 'styled-components'
+
+const Cursor = styled.div.attrs(props => ({
+    style: {
+        left: props.clientX - 20,
+        top: props.clientY - 20
+    },
+}))`
+    position: fixed;
+    z-index: 9999;
+    pointer-events: none;
+    border-radius: 50%;
+    border: 1px solid #E2E2E2;
+    width: 35px;
+    height: 35px;
+    `
 
 const CustomCursor = () => {
 
     const { clientX, clientY } = useMousePosition()
 
     return (
-        <div style={{
-            position: "fixed",
-            top: clientY - 20,
-            left: clientX - 20,
-            zIndex: 9999,
-            pointerEvents: "none",
-            borderRadius: "50%",
-            border: "1px solid #E2E2E2",
-            width: "35px",
-            height: "35px"
-        }} className="cursor">
+        <Cursor className="cursor" clientY={clientY} clientX={clientX}>
             <svg style={{ position: "absolute", top: -8, left: -8 }}></svg>
-        </div>
+        </Cursor>
     )
 }
 
