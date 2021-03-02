1461e39bcfd9183c5a50ecdc1377b9ae713e7eb2
ajiiz <opticpiter@gmail.com>
2021-03-02 18:23:09 +0100

    Added schema for post

--- backend/models/postMessage.js
+++ backend/models/postMessage.js
@@ -1,1 +1,14 @@
 import mongoose from 'mongoose'
+
+const postSchema = mongoose.Schema({
+    title: String,
+    content: String,
+    image:
+    {
+        data: Buffer,
+        contentType: String
+    }
+})
+
+const PostMessage = mongoose.model('PostMessage', postSchema)
+export default PostMessage
