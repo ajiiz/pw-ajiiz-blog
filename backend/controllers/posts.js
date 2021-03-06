39e1f69299a77820c854ca95f94a3f705a420d8f
ajiiz <opticpiter@gmail.com>
2021-03-06 22:40:38 +0100

    Added getPosts async function

--- backend/controllers/posts.js
+++ backend/controllers/posts.js
@@ -1,5 +1,11 @@
 import PostMessage from "../models/postMessage.js"
 
-export const getPosts = (req, res) => {
-    res.send('THIS WORKS')
+export const getPosts = async (req, res) => {
+    try {
+        const postMessages = await PostMessage.find()
+        console.log(postMessages)
+        res.status(200).json(postMessages)
+    } catch (error) {
+
+    }
 }
