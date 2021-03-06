b3b70de2316c2f2be8cb1422dbbd35b4e56ad8b1
ajiiz <opticpiter@gmail.com>
2021-03-06 23:36:33 +0100

    Fixed spacing and added comment

--- frontend/src/reducers/index.js
+++ frontend/src/reducers/index.js
@@ -1,6 +1,4 @@
 import { combineReducers } from "redux"
 import posts from "./posts"
 
-export default combineReducers({
-    posts,
- })
+export default combineReducers({ posts }) // posts:posts is same as posts because key and the value are the same
