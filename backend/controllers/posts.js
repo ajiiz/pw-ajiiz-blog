e8086fdbdce435accdd918afc96acf21959ac40c
ajiiz <opticpiter@gmail.com>
2021-03-06 22:45:40 +0100

    Added createPost async function

--- backend/controllers/posts.js
+++ backend/controllers/posts.js
@@ -6,6 +6,18 @@ export const getPosts = async (req, res) => {
         console.log(postMessages)
         res.status(200).json(postMessages)
     } catch (error) {
+        res.status(404).json({ message: error.message })
+    }
+}
 
+export const createPost = async (res, req) => {
+    const post = req.body
+    const newPost = new PostMessage(post)
+
+    try {
+        await newPost.save()
+        res.status(201).json(newPost)
+    } catch (error) {
+        res.status(409).json({ message: error.message })
     }
 }
