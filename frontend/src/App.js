feb8e469fe3498c08b997b5567ab47d81a84b7e6
ajiiz <opticpiter@gmail.com>
2021-03-07 14:15:36 +0100

    Added dispatch hook

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,22 @@
-import React from "react"
+import React, { useEffect } from "react"
 import { Route, BrowserRouter as Router, Switch, Redirect } from "react-router-dom"
+import { useDispatch } from "react-redux"
 import Home from "./components/HomePage/Home"
 import Login from "./components/LoginPage/Login"
 import Blog from "./components/BlogPage/Blog"
 import Post from "./components/PostPage/Post"
 import "./styles/main.scss"
 import CustomCursor from "./components/CustomCursor/CustomCursor"
+import { getPosts } from "./actions/posts"
 
 const App = () => {
+
+    const dispatch = useDispatch()
+
+    useEffect(() => {
+        dispatch(getPosts())
+    }, [dispatch])
+
     return (
         <>
             <CustomCursor />
