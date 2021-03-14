91893a74ad9337faaa2acd2a66dd2679a1ed0b15
ajiiz <opticpiter@gmail.com>
2021-03-14 14:48:20 +0100

    Added conditional rendering for posts

--- frontend/src/components/BlogPage/Blog.js
+++ frontend/src/components/BlogPage/Blog.js
@@ -55,118 +55,46 @@ const Blog = () => {
                     <div className="blog__wrapper__header">
                         <p className="blog__wrapper__header__text">Welcome to my madness</p>
                     </div>
-                    <div className="blog__wrapper__posts">
-                    {/* There will be mapping through posts */}
-                        <div className="blog__wrapper__posts__post">
-                            <div className="blog__wrapper__posts__post__image">
-                                <img alt="post" src={testImage}></img>
-                            </div>
-                            <div className="blog__wrapper__posts__post__content">
-                                <div className="blog__wrapper__posts__post__content__date">
-                                    <div className="blog__wrapper__posts__post__content__date__divider"></div>
-                                    <p className="blog__wrapper__posts__post__content__date__text">16.02.21</p>
-                                    <div className="blog__wrapper__posts__post__content__date__divider"></div>
+                    {!posts.length ? <p>Posts missing</p> : (
+                        <div className="blog__wrapper__posts">
+                            <div className="blog__wrapper__posts__post">
+                                <div className="blog__wrapper__posts__post__image">
+                                    <img alt="post" src={testImage}></img>
                                 </div>
-                                <p className="blog__wrapper__posts__post__content__title">Black Roses Title</p>
-                                <p className="blog__wrapper__posts__post__content__details">
-                                    “The Laughing Heart
-                                    your life is your life
-                                    don’t let it be clubbed into dank submission.
-                                    be on the watch.
-                                    there are ways out.
-                                    there is a light somewhere.
-                                    it may not be much light but
-                                    it beats the darkness.
-                                    be on the watch.
-                                    the gods will offer you chances.
-                                    know them.
-                                    take them.
-                                    you can’t beat death but
-                                    you can beat death in life, sometimes.
-                                    and the more often you learn to do it,
-                                    the more light there will be.
-                                    your life is your life.
-                                    know it while you have it.
-                                    you are marvelous
-                                    the gods wait to delight
-                                    in you.”
-                                </p>
+                                <div className="blog__wrapper__posts__post__content">
+                                    <div className="blog__wrapper__posts__post__content__date">
+                                        <div className="blog__wrapper__posts__post__content__date__divider"></div>
+                                        <p className="blog__wrapper__posts__post__content__date__text">16.02.21</p>
+                                        <div className="blog__wrapper__posts__post__content__date__divider"></div>
+                                    </div>
+                                    <p className="blog__wrapper__posts__post__content__title">Black Roses Title</p>
+                                    <p className="blog__wrapper__posts__post__content__details">
+                                        “The Laughing Heart
+                                        your life is your life
+                                        don’t let it be clubbed into dank submission.
+                                        be on the watch.
+                                        there are ways out.
+                                        there is a light somewhere.
+                                        it may not be much light but
+                                        it beats the darkness.
+                                        be on the watch.
+                                        the gods will offer you chances.
+                                        know them.
+                                        take them.
+                                        you can’t beat death but
+                                        you can beat death in life, sometimes.
+                                        and the more often you learn to do it,
+                                        the more light there will be.
+                                        your life is your life.
+                                        know it while you have it.
+                                        you are marvelous
+                                        the gods wait to delight
+                                        in you.”
+                                    </p>
+                                </div>
                             </div>
                         </div>
-                        <div className="blog__wrapper__posts__post">
-                            <div className="blog__wrapper__posts__post__image">
-                                <img alt="post" src={testImage}></img>
-                            </div>
-                            <div className="blog__wrapper__posts__post__content">
-                                <div className="blog__wrapper__posts__post__content__date">
-                                    <div className="blog__wrapper__posts__post__content__date__divider"></div>
-                                    <p className="blog__wrapper__posts__post__content__date__text">16.02.21</p>
-                                    <div className="blog__wrapper__posts__post__content__date__divider"></div>
-                                </div>
-                                <p className="blog__wrapper__posts__post__content__title">Black Roses Title</p>
-                                <p className="blog__wrapper__posts__post__content__details">
-                                    “The Laughing Heart
-                                    your life is your life
-                                    don’t let it be clubbed into dank submission.
-                                    be on the watch.
-                                    there are ways out.
-                                    there is a light somewhere.
-                                    it may not be much light but
-                                    it beats the darkness.
-                                    be on the watch.
-                                    the gods will offer you chances.
-                                    know them.
-                                    take them.
-                                    you can’t beat death but
-                                    you can beat death in life, sometimes.
-                                    and the more often you learn to do it,
-                                    the more light there will be.
-                                    your life is your life.
-                                    know it while you have it.
-                                    you are marvelous
-                                    the gods wait to delight
-                                    in you.”
-                                </p>
-                            </div>
-                        </div>
-                        <div className="blog__wrapper__posts__post">
-                            <div className="blog__wrapper__posts__post__image">
-                                <img alt="post" src={testImage}></img>
-                            </div>
-                            <div className="blog__wrapper__posts__post__content">
-                                <div className="blog__wrapper__posts__post__content__date">
-                                    <div className="blog__wrapper__posts__post__content__date__divider"></div>
-                                    <p className="blog__wrapper__posts__post__content__date__text">16.02.21</p>
-                                    <div className="blog__wrapper__posts__post__content__date__divider"></div>
-                                </div>
-                                <p className="blog__wrapper__posts__post__content__title">Black Roses Title</p>
-                                <p className="blog__wrapper__posts__post__content__details">
-                                    “The Laughing Heart
-                                    your life is your life
-                                    don’t let it be clubbed into dank submission.
-                                    be on the watch.
-                                    there are ways out.
-                                    there is a light somewhere.
-                                    it may not be much light but
-                                    it beats the darkness.
-                                    be on the watch.
-                                    the gods will offer you chances.
-                                    know them.
-                                    take them.
-                                    you can’t beat death but
-                                    you can beat death in life, sometimes.
-                                    and the more often you learn to do it,
-                                    the more light there will be.
-                                    your life is your life.
-                                    know it while you have it.
-                                    you are marvelous
-                                    the gods wait to delight
-                                    in you.”
-                                </p>
-                            </div>
-                        </div>
-                    {/* ------------ */}
-                    </div>
+                    )}
                     <div className="blog__wrapper__scroll-up" onClick={scrollTop} style={(isScrollUp) ? {opacity: 1, visibility: "visible"} : null}>
                         <FaArrowUp style={arrowUpStyle}/>
                         <p className="blog__wrapper__scroll-up__text">scroll up</p>
