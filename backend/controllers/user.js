39a93964c52a136d82c1d74f20a69d4d3104cf34
ajiiz <opticpiter@gmail.com>
2021-03-29 19:14:06 +0200

    Added verification if user exists in db

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -4,5 +4,13 @@ import UserModel from "../models/user.js"
 
 export const signin = async (req, res) => {
     const { username, password } = req.body;
+    try {
+        const oldUser = await UserModel.findOne({ username });
 
+        if (!oldUser) return res.status(404).json({ message: "User doesn't exist" });
+
+
+      } catch (e) {
+        res.status(500).json({ message: "Something went wrong" + e });
+      }
   };
