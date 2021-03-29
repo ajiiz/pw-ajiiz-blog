2ccd040957befd53b788b1f466375699b75e45eb
ajiiz <opticpiter@gmail.com>
2021-03-29 19:20:13 +0200

    Added id to schema

--- backend/models/user.js
+++ backend/models/user.js
@@ -3,6 +3,7 @@ import mongoose from 'mongoose'
 const userSchema = mongoose.Schema({
     username: { type: String, required:  true },
     password: { type: String, required: true },
+    id: { type: String },
 })
 
 const user = mongoose.model('User', userSchema)
