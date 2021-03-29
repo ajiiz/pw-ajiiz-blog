dc0e2d2d2faa352a312e2f2cbbf70227fb92360b
ajiiz <opticpiter@gmail.com>
2021-03-29 18:38:20 +0200

    Added user route

--- backend/index.js
+++ backend/index.js
@@ -13,6 +13,7 @@ app.use(express.urlencoded({ limit: "30mb", extended: true}))
 app.use(cors())
 
 app.use('/posts', postRoutes)
+app.use('/user', userRoutes)
 
 const CONNECTION_URL = process.env.CONNECTION_URL
 const PORT = process.env.PORT || 5000
