6dd8c156b172cca9ee26dc528840dda1f36c8428
ajiiz <opticpiter@gmail.com>
2021-03-29 18:39:17 +0200

    Imported users routes

--- backend/index.js
+++ backend/index.js
@@ -2,7 +2,9 @@ import express from 'express'
 import mongoose from 'mongoose'
 import cors from 'cors'
 import dotenv from 'dotenv'
+
 import postRoutes from './routes/posts.js'
+import userRoutes from './routes/users.js'
 
 //Initialize app
 const app = express()
