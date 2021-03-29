af7642c2799125803621afa7e558eb2b7f1ee929
ajiiz <opticpiter@gmail.com>
2021-03-29 18:53:22 +0200

    Removed id from schema

--- backend/models/user.js
+++ backend/models/user.js
@@ -3,7 +3,6 @@ import mongoose from 'mongoose'
 const userSchema = mongoose.Schema({
     name: { type: String, required:  true },
     password: { type: String, required: true },
-    id: { type: String },
 })
 
 const user = mongoose.model('User', user)
