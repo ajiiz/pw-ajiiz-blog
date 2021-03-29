181f4fcc30976c95fa47b6aebb053f2686967950
ajiiz <opticpiter@gmail.com>
2021-03-29 19:18:40 +0200

    Added password verification, changed oldUser to existingUser

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -3,14 +3,15 @@ import jwt from "jsonwebtoken";
 import UserModel from "../models/user.js"
 
 export const signin = async (req, res) => {
-    const { username, password } = req.body;
+    const { username, password } = req.body
     try {
-        const oldUser = await UserModel.findOne({ username });
+        const existingUser = await UserModel.findOne({ username })
+        if (!existingUser) return res.status(404).json({ message: "User doesn't exist" })
 
-        if (!oldUser) return res.status(404).json({ message: "User doesn't exist" });
-
+        const isPasswordCorrect = await password == existingUser.password
+        if (!isPasswordCorrect) return res.status(400).json({ message: "Invalid password" })
 
       } catch (e) {
-        res.status(500).json({ message: "Something went wrong" + e });
+        res.status(500).json({ message: "Something went wrong" + e })
       }
   };
