86c648f9b9a30faae29c02945284c765e3cdcfbf
ajiiz <opticpiter@gmail.com>
2021-03-30 18:59:17 +0200

    Added middleware auth functionality and jwt verification

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -1,5 +1,18 @@
 import jwt from "jsonwebtoken"
 
 const auth = async (req, res, next) => {
+    try {
+        const token = req.headers.authorization.split(" ")[1]
+        const secret = process.env.SECRET_TOKEN
 
+        const decodedData = jwt.verify(token, secret)
+
+        req.userId = decodedData?.id
+
+        next()
+    } catch (e) {
+        console.log(e)
+    }
 }
+
+export default auth
