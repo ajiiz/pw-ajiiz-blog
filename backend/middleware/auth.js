db1fa6aa6ae9fac64b9adebd0f1d2aafd59f7023
ajiiz <opticpiter@gmail.com>
2021-03-30 23:28:07 +0200

    Modified the middleware - need to fix the headers.authorization that returns undefined

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -2,16 +2,15 @@ import jwt from "jsonwebtoken"
 
 const auth = async (req, res, next) => {
     try {
-        const token = req.headers.authorization.split(" ")[1]
+        const token = req.headers.authorization.split(' ')[1]
         const secret = process.env.SECRET_TOKEN
-
-        const decodedData = jwt.verify(token, secret)
+        let decodedData = jwt.verify(token, secret)
 
         req.userId = decodedData?.id
 
         next()
     } catch (e) {
-        console.log(e)
+        res.status(500).json({ message: "Something went wrong. " + e })
     }
 }
 
