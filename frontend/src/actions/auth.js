fc31f5705115ba7c086b73f18a25958b606ba834
ajiiz <opticpiter@gmail.com>
2021-03-30 23:25:15 +0200

    Modified auth action

--- frontend/src/actions/auth.js
+++ frontend/src/actions/auth.js
@@ -3,10 +3,10 @@ import * as api from '../api/index.js'
 export const signin = (formData, router) => async (dispatch) => {
     try {
       const { data } = await api.signIn(formData)
-      dispatch({ type: "AUTH", data })
+      dispatch({ type: "AUTH", payload: data })
       router.push('/post')
 
     } catch (e) {
       console.log(e)
     }
-  };
+  }
