f5aa4296d745def20a16060a839a699a5f3053b1
ajiiz <opticpiter@gmail.com>
2021-03-30 18:50:37 +0200

    Modified secret token value - now its in env file, fixed spaces

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -3,21 +3,22 @@ import bcrypt from "bcryptjs"
 
 import UserModel from "../models/user.js"
 
-const secret = "User passed - secret"
-
 export const signin = async (req, res) => {
-    const { username, password } = req.body
-    try {
-        const existingUser = await UserModel.findOne({ username })
-        if (!existingUser) return res.status(404).json({ message: "User doesn't exist" })
 
-        const isPasswordCorrect = await bcrypt.compare(password, existingUser.password)
-        if (!isPasswordCorrect) return res.status(400).json({ message: "Invalid password" })
+  const { username, password } = req.body
+  const secret = process.env.SECRET_TOKEN
 
-        const token = jwt.sign({ username: existingUser.username, id: existingUser._id }, secret, { expiresIn: "2h" })
-        res.status(200).json({ result: existingUser, token })
+  try {
+    const existingUser = await UserModel.findOne({ username })
+    if (!existingUser) return res.status(404).json({ message: "User doesn't exist" })
 
-      } catch (e) {
-        res.status(500).json({ message: "Something went wrong. " + e })
-      }
-  };
+    const isPasswordCorrect = await bcrypt.compare(password, existingUser.password)
+    if (!isPasswordCorrect) return res.status(400).json({ message: "Invalid password" })
+
+    const token = jwt.sign({ username: existingUser.username, id: existingUser._id }, secret, { expiresIn: "2h" })
+    res.status(200).json({ result: existingUser, token })
+
+    } catch (e) {
+      res.status(500).json({ message: "Something went wrong. " + e })
+    }
+}
