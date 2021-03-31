94e190affcec65940e8301c10fb3157deaf884ee
ajiiz <opticpiter@gmail.com>
2021-03-31 23:02:45 +0200

    Properly working auth middleware

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -2,12 +2,11 @@ import jwt from "jsonwebtoken"
 
 const auth = async (req, res, next) => {
     try {
-        const token = req.headers.authorization.split(' ')[1]
+        const authHeader = req.headers['authorization']
+        const token = authHeader.split(' ')[1]
         const secret = process.env.SECRET_TOKEN
         let decodedData = jwt.verify(token, secret)
-
         req.userId = decodedData?.id
-
         next()
     } catch (e) {
         res.status(500).json({ message: "Something went wrong. " + e })
