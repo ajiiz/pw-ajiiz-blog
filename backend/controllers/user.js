0000de020dab100d351f8b40a65c0dfb8f0b27a6
ajiiz <opticpiter@gmail.com>
2021-03-31 23:03:39 +0200

    Shorten the res properties (token)

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -16,7 +16,7 @@ export const signin = async (req, res) => {
     if (!isPasswordCorrect) return res.status(400).json({ message: "Invalid password" })
 
     const token = jwt.sign({ username: existingUser.username, id: existingUser._id }, secret, { expiresIn: "2h" })
-    res.status(200).json({ result: existingUser, token })
+    res.json({ result: existingUser, token })
 
     } catch (e) {
       res.status(500).json({ message: "Something went wrong. " + e })
