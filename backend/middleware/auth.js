dafd06cc0b7f601db5bbe39c466effa83350ac13
ajiiz <opticpiter@gmail.com>
2021-03-31 23:04:30 +0200

    Added comment - optiona chaining

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -6,7 +6,7 @@ const auth = async (req, res, next) => {
         const token = authHeader.split(' ')[1]
         const secret = process.env.SECRET_TOKEN
         let decodedData = jwt.verify(token, secret)
-        req.userId = decodedData?.id
+        req.userId = decodedData?.id // Optional_chaining
         next()
     } catch (e) {
         res.status(500).json({ message: "Something went wrong. " + e })
